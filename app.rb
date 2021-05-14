03510273d62570d287afb27119e1683cf2724e2e
Tim Quimbo <tquimbo1@gmail.com>
2021-05-13 20:24:03 -0400

    commit tree e5984ba1633e9b3a40c9b699b3fd42e33b1e606e

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/" do
+        erb :user_input
+
+    end
+
+    post '/piglatinize' do
+    
+    end
+
+
+    post '/piglatinize' do
+    
+    end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,9 @@
+class PigLatinizer
+
+    attr_accessor :word
+
+    def initialize
+    
+    end
+
+end
